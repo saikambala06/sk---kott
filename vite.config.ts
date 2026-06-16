a6073625b22b69e4dd48bfa79925b7aa01ef536c
saikambala06 <saikambala777@gmail.com>
2026-06-16 17:29:54 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
 import path from 'path';
+
+// Calculate __dirname for ESM
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
